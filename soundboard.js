ef2fb6e34fbfe64deda15178518f0b323f93552a
AlecTryan <90415618+AlecTryan@users.noreply.github.com>
2021-11-18 16:24:24 -0500

    Update soundboard.js

--- soundboard.js
+++ soundboard.js
@@ -1,1 +1,15 @@
+var buttonFirst = document.getElementById('my-first-button')
+var buttonSecond = document.getElementById('my-second-button')
+var buttonThird = document.getElementById('my-third-button')
 
+buttonFirst.addEventListener('click', function () {
+  console.log('I was clicked!')
+})
+
+buttonSecond.addEventListener('click', function () {
+  console.log('I was clicked!')
+})
+
+buttonThird.addEventListener('click', function () {
+  console.log('I was clicked!')
+})
